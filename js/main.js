81a135d7a1ee460bca81405014cd89e037ba429d
tungithut <tungithut@gmail.com>
2013-05-22 14:50:36 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
 var app = {
 
-    renderHomeView: function() {
-        /*
-        var html =
-                "<div class='header'><h1>Home</h1></div>" +
-                "<div class='search-view'>" +
-                "<input class='search-key'/>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>"
-        $('body').html(html);
-        */
+    /*renderHomeView: function() {
         $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
+    },*/
     
     showAlert: function (message, title) {
         if (navigator.notification) {
@@ -22,24 +13,13 @@ var app = {
         }
     },
     
-    findByName: function() {
+    /*findByName: function() {
         console.log('findByName');
-        /*this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });*/
-        
         var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
         $('.employee-list').html(self.employeeLiTpl(employees));
-    });
-        
-    },
+        });
+    },*/
 
     /*initialize: function() {
         this.store = new MemoryStore();
@@ -47,19 +27,20 @@ var app = {
     }*/
     
     initialize: function() {
-        var self = this;
+        /*var self = this;
         this.store = new MemoryStore(function() {
             //self.showAlert('Store Initialized', 'Info');
             self.renderHomeView();
+        });*/
+        var self = this;
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
         });
         
         this.homeTpl = Handlebars.compile($("#home-tpl").html());
         this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
-    
-}
 
 };
 
